db44159266719c64b4cf8caac90423a4121f06b9
WaddJaloloff <fazliddindjalolov77@gmail.com>
2024-07-17 16:18:31 +0500

    Update urls.py

--- application/urls.py
+++ application/urls.py
@@ -4,11 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('post/<int:post_id>/', views.post_detail, name='post_detail'),
-
-    path('add_to_cart/', views.add_to_cart, name='add_to_cart'),
+    path('add-to-cart/', views.add_to_cart, name='add_to_cart'),
+    path('cart-count/', views.cart_count, name='cart_count'),
     path('update_cart/<int:post_id>/<str:action>/', views.update_cart, name='update_cart'),
     path('clear_cart/', views.clear_cart, name='clear_cart'),
-    path('remove_from_cart/<int:post_id>/', views.remove_from_cart, name='remove_from_cart'),
-    path('order/', views.order, name='order'),
     path('place_order/', views.place_order, name='place_order'),
+    path('order/', views.order, name='order'),
+    path('remove_from_cart/<int:post_id>/', views.remove_from_cart, name='remove_from_cart'),
 ]
